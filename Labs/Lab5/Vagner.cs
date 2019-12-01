eb67a561a5c55dd0fa5987ec3fbc643ff1c1054b
Vladislav Alpeev <vlad.alpeev@mail.ru>
2019-12-01 14:58:45 +0300

    added Wagner-Fisher algorithm

---
+++ Labs/Lab5/Vagner.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lab5
+{
+    public class Vagner
+    {
+        public int findDistance(string str1, string str2)
+        {
+            int str1Len = str1.Length;
+            int str2Len = str2.Length;
+
+            if ((str1 == null) || (str2 == null)) return -1; //случай, когда одно из слов не введено
+            if ((str1Len == 0) && (str2Len == 0)) return 0; //случай, когда строки нулевые
+            if (str1Len == 0) return str2Len; // -//- только одна строка нулевая
+            if (str2Len == 0) return str1Len;
+
+            str1.ToUpper();
+            str2.ToUpper();
+
+            int[,] matr = new int[str1Len + 1, str2Len + 1]; //матрица
+            for (int i = 0; i <= str1Len; i++) matr[i, 0] = i; //заполнение нулевой строки и столбцов
+            for (int j = 0; j <= str2Len; j++) matr[0, j] = j;
+            for(int i = 1; i <= str2Len; i++) 
+            { 
+                for (int j = 1; j <= str2Len; j++)
+                {
+                    //Левенштайн
+                    int sEq = ((str1.Substring(i - 1, 1) == str2.Substring(j - 1, 1)) ? 0 : 1);
+                    int ins = matr[i, j - 1] + 1; //добавление
+                    int del = matr[i - 1, j] + 1; //удаление
+                    int subst = matr[i - 1, j - 1] + sEq;
+                    matr[i, j] = Math.Min(Math.Min(ins, del), sEq); //выбор минимальной длины
+                    
+                    //Дамерау
+                    if((i > 1) && (str1.Substring(i - 1, 1) == str2.Substring(j - 2, 1)) &&
+                    (str1.Substring(i - 2, 1) == str2.Substring(j - 1, 1)))
+                    {
+                        matr[i, j] = Math.Min(matr[i, j],
+                        matr[i - 2, j - 2] + sEq);
+                    }
+                }
+            }
+            return matr[str1Len, str2Len]; //итоговое расстрояние
+        }
+    }
+}
